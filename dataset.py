350d21ec524f49d7c4be89ea5b50f93eb50cce33
klatyma <ardaturkyasar@gmail.com>
2022-07-22 00:07:50 +0300

    updated CustomImageDataset

--- dataset.py
+++ dataset.py
@@ -5,22 +5,21 @@ import cv2 as cv
 from torch.utils.data import Dataset
 
 class CustomImageDataset(Dataset):
-    def __init__(self, img_dir, depth_dir,img_labels, transform=None, target_transform=None):
+    def __init__(self, img_dir, depth_dir, transform=None, target_transform=None):
         self.depth_dir = depth_dir
         self.img_dir = img_dir
-        self.img_labels = pd.read_csv(img_labels)
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(os.listdir(self.img_dir))
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = cv.imread(img_path, cv.IMREAD_REDUCED_COLOR_2 + cv.IMREAD_ANYDEPTH)
-        label = self.img_labels.iloc[idx, 1]
+        img_path = os.path(self.img_dir + '/' + os.listdir(self.img_dir)[idx])
+        depth_path = os.path(self.depth_dir + '/' + os.listdir(self.depth_dir)[idx])
+        image = cv.imread(img_path, cv.IMREAD_ANYDEPTH)
+        depth = cv.imread(depth_path, cv.IMREAD_ANYDEPTH)
         if self.transform:
             image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+            depth = self.transform(depth)
+        return image, depth
