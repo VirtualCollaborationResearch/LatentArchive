5ea25c90975998772b7382ca8ed28a3413b19d02
klatyma <ardaturkyasar@gmail.com>
2022-08-16 00:02:12 +0300

    testing trainer for fine tuning

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,10 +1,12 @@
 # %%
 import numpy as np
+from sympy import print_gtk
 import torch
 import torchvision.transforms as transforms
 import cv2 as cv
 import matplotlib.pyplot as plt
 from skimage import exposure
+from PIL import Image
 from skimage.exposure import match_histograms
 import os
 
@@ -35,24 +37,31 @@ def plot_histogram(img,title, bins = 100):
 def rgb_transform(img):
     """Function that takes a tensor and returns it in gray scale and with additive noise"""
     transform = transforms.Compose([
-                                    #transforms.ToTensor(),
-                                    transforms.Grayscale(num_output_channels=1),
+                                    transforms.ToTensor(),
+                                    transforms.Grayscale(num_output_channels=3),
                                     transforms.GaussianBlur(kernel_size=(7,13), sigma=(2, 4)),
                                     #transforms.RandomSolarize(threshold= 200, p=0.3),
                                     #transforms.Normalize((0.5), (0.5)), #May not be required after histogram matching
-                                    AddGaussianNoise(mean=0.0, std=0.1)])
-    img = img.transpose(0,2).transpose(1,2)
+                                    AddGaussianNoise(mean=0.0, std=0.1)
+                                    #transforms.ToPILImage()
+                                    ])
+    #img = img.transpose(0,2).transpose(1,2)
     img = transform(img)
     return img
 
-def histogram_matching(img, ref_dir):
+def histogram_matching(img, ref_dir='./Manhatta Frames/'):
     """Function that matches histogram of a given image with all images in a given directory"""
-    img = img.numpy()
+    img = np.array(img)
     random_images = np.random.choice(os.listdir(ref_dir), size=5)
     for ref in random_images:
         ref_path = ref_dir + '/' + ref
         ref_img = cv.imread(ref_path, cv.IMREAD_ANYDEPTH + cv.IMREAD_GRAYSCALE)
         ref_img = ref_img.reshape(ref_img.shape[0],ref_img.shape[1],1)
         img = match_histograms(img, ref_img, multichannel=False)
+    img = torch.from_numpy(img)
+    #img = Image.fromarray(img)
     return img
 
+def transform_to_PIL(img):
+    transform = transforms.ToPILImage()
+    return transform(img)
--- dataset.py
+++ dataset.py
@@ -4,10 +4,11 @@ import cv2 as cv
 from torch.utils.data import Dataset
 
 class CustomImageDataset(Dataset):
-    def __init__(self, img_dir, depth_dir, transform=None):
+    def __init__(self, img_dir, depth_dir, transform=None, histogram_matching=None):
         self.depth_dir = depth_dir
         self.img_dir = img_dir
         self.transform = transform
+        self.histogram_matching = histogram_matching
 
     def __len__(self):
         return len(os.listdir(self.img_dir))
@@ -17,7 +18,9 @@ class CustomImageDataset(Dataset):
         depth_path = self.depth_dir + '/' + os.listdir(self.depth_dir)[idx]
         image = cv.imread(img_path, cv.IMREAD_ANYDEPTH + cv.IMREAD_COLOR)
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-        depth = cv.imread(depth_path, cv.IMREAD_ANYDEPTH)
+        depth = cv.imread(depth_path, cv.IMREAD_ANYDEPTH + cv.IMREAD_GRAYSCALE)
         if self.transform:
             image = self.transform(image)
+        if self.histogram_matching:
+            image = self.histogram_matching(image)
         return image, depth
